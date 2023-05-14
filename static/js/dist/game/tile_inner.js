f1e63fc7d4cbaa1bbf206b024df4e31802f6eacd
Samuel Risner <96385448+Samuel-Risner@users.noreply.github.com>
2023-05-14 22:08:40 +0200

    Fixed a bug
    Forgot to add the line for adding questionmarks to the stats counter.

--- static/js/dist/game/tile_inner.js
+++ static/js/dist/game/tile_inner.js
@@ -333,6 +333,7 @@ export class TileInner extends TileParent {
                 if ((this.field.getMode() === 1) || !leftClick) {
                     this.setQuestionMark();
                     this.stats.removeFlag(this.mode === -1);
+                    this.stats.addQuestionMark();
                 }
                 break;
             case 3: // The tile is revealed:
