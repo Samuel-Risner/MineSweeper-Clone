4aff7c3b310fb3dbe6ee4d3533c29ac49c09d6e4
Samuel Risner <96385448+Samuel-Risner@users.noreply.github.com>
2023-05-22 20:35:13 +0200

    Added my new ipv4 address

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ def index():
     return render_template("index.html")
 
 # app.run(port=5000, host="192.168.1.61", debug=True)
-app.run(port=5000, host="127.0.0.1", debug=True)
+# app.run(port=5000, host="127.0.0.1", debug=True)
 # app.run(port=5000, host="192.168.178.137", debug=True)
 # app.run(port=5000, host="192.168.0.27", debug=True)
+app.run(port=5000, host="192.168.1.59", debug=True)
