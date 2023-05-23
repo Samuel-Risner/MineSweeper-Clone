ca39989b42e26b3a58adea516122f444fc7da290
Samuel Risner <96385448+Samuel-Risner@users.noreply.github.com>
2023-05-24 00:51:35 +0200

    Reoved favicon route

--- main.py
+++ main.py
@@ -6,8 +6,4 @@ app = Flask(__name__, template_folder="./minesweeper-clone/", static_folder="min
 def index():
     return render_template("index.html")
 
-@app.route("/favicon.ico")
-def favicon():
-    return send_file("minesweeper-clone/favicon.ico", mimetype="image/ico")
-
 app.run(port=5000, host="127.0.0.1", debug=True)
