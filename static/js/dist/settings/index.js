93212c1820065e16c09952847f6f41161f3c315e
Samuel Risner <96385448+Samuel-Risner@users.noreply.github.com>
2023-06-26 17:50:08 +0200

    Removed old code

--- static/js/dist/settings/index.js
+++
@@ -1,9 +0,0 @@
-import { defaultFieldSizes } from "./defaultFieldSizes.js";
-import { field } from "./field.js";
-import { tile } from "./tile.js";
-const settings = {
-    defaultFieldSizes: defaultFieldSizes,
-    field: field,
-    tile: tile
-};
-export default settings;
